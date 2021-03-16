d5c2dea10fcccf1dc8639f3fae21a56d1bc49580
Reza <reza.jabal@einstein.yu.edu>
2021-03-16 16:59:22 -0400

    Update PRS_calculation.sh

--- PRS_calculation.sh
+++ PRS_calculation.sh
@@ -34,31 +34,31 @@ cat list_of_summary_statistic.txt | while read i
 
 
 		${PATH_to_PRSice} \
-		--A1 ALT \
-               	--A2 REF \
-               	--bar-levels 1e-40,1e-30,1e-20,1e-10,1e-09,1e-08,1e-07,1e-06,1e-05,0.0001,0.0002,0.0005,0.001,0.002,0.005,0.01,0.05,0.1,1 \
-               	--base ${i}.assoc \
-               	--beta  \
-               	--binary-target F \
-               	--bp BP \
-               	--chr CHR \
-               	--clump-kb 250 \
-               	--clump-p 1.000000 \
-               	--clump-r2 0.100000 \
-               	--interval 5e-05 \
-               	--lower 5e-08 \
-               	--missing MEAN_IMPUTE \
-               	--model add \
-               	--out ${out}/${i}_prs \
-               	--perm 10000 \
-               	--pvalue P-value \
-               	--score avg \
-               	--seed 910185829 \
-               	--snp CHR:BP \
-               	--stat Effect \
-               	--target ${target} \
-               	--thread 1 \
-               	--upper 0.5
+			--A1 ALT \
+               		--A2 REF \
+               		--bar-levels 1e-40,1e-30,1e-20,1e-10,1e-09,1e-08,1e-07,1e-06,1e-05,0.0001,0.0002,0.0005,0.001,0.002,0.005,0.01,0.05,0.1,1 \
+               		--base ${i}.assoc \
+               		--beta  \
+               		--binary-target F \
+               		--bp BP \
+               		--chr CHR \
+               		--clump-kb 250 \
+               		--clump-p 1.000000 \
+               		--clump-r2 0.100000 \
+               		--interval 5e-05 \
+               		--lower 5e-08 \
+               		--missing MEAN_IMPUTE \
+               		--model add \
+               		--out ${out}/${i}_prs \
+               		--perm 10000 \
+               		--pvalue P-value \
+               		--score avg \
+               		--seed 910185829 \
+               		--snp CHR:BP \
+               		--stat Effect \
+               		--target ${target} \
+               		--thread 1 \
+               		--upper 0.5
 
 rm ${i}.assoc xx
 gzip ${i}
